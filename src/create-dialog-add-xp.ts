8e1760ef30b11e468defff24a34b496adfd61268
th3mon <p.szelenberger@gmail.com>
2023-08-23 21:56:26 +0200

    Move texts into lang files

--- src/create-dialog-add-xp.ts
+++ src/create-dialog-add-xp.ts
@@ -2,30 +2,37 @@ export const createDialogAddXP = (
   add: () => void,
   cancel: () => void
 ): Dialog => {
-  // TODO: Move texts into lang files
+  const xp = game.i18n.localize('wfrp4e.add-xp.modal.content.xp');
+  const reason = game.i18n.localize('wfrp4e.add-xp.modal.content.reason');
+  const title = game.i18n.localize('wfrp4e.add-xp.modal.title');
+  const okLabel = game.i18n.localize('wfrp4e.add-xp.modal.button.ok.label');
+  const cancelLabel = game.i18n.localize(
+    'wfrp4e.add-xp.modal.button.cancel.label'
+  );
+
   const content = `
     <div class="form-group">
-    <label for="xp">XP</label>
-    <input type="number" id="xp" name="xp">
+      <label for="xp">${xp}</label>
+      <input type="number" id="xp" name="xp">
     </div>
     <div class="form-group">
-    <label for="reason">Reason</label>
-    <input type="text" id="reason" name="reason">
+      <label for="reason">${reason}</label>
+      <input type="text" id="reason" name="reason">
     </div>
   `;
 
   return new Dialog({
-    title: 'Add XP',
+    title,
     content,
     buttons: {
       add: {
         icon: '<i class="fas fa-check"></i>',
-        label: game.i18n.localize('wfrp4e.add-xp.modal.button.ok.label'),
+        label: okLabel,
         callback: add,
       },
       cancel: {
         icon: '<i class="fas fa-times"></i>',
-        label: game.i18n.localize('wfrp4e.add-xp.modal.button.cancel.label'),
+        label: cancelLabel,
         callback: cancel,
       },
     },
