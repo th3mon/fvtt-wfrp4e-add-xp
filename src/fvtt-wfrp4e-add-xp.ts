c53720992d758cbe14b6ac992f708e55c955fd56
th3mon <p.szelenberger@gmail.com>
2023-08-24 00:28:39 +0200

    Create registerGlobals()

--- src/fvtt-wfrp4e-add-xp.ts
+++ src/fvtt-wfrp4e-add-xp.ts
@@ -1,7 +1,6 @@
-import { main } from './main';
 import './fvtt-wfrp4e-add-xp.scss';
+import { registerGlobals } from './register-globals';
 
 Hooks.on('ready', function () {
-  // TODO: Move this code into function. I'm sure the name should be register global(s)
-  globalThis.wfrpAddXp = main;
+  registerGlobals();
 });
