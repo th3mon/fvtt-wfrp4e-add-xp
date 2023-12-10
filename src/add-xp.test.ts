7dc2f3adeebb7b19e2586fe7e19b181ccc0808e5
th3mon <p.szelenberger@gmail.com>
2023-12-10 18:34:06 +0100

    Add Ambitions XP

--- src/add-xp.test.ts
+++ src/add-xp.test.ts
@@ -115,4 +115,110 @@ describe('addXP', () => {
 
     addXP(selectedPlayers);
   });
+
+  it('add short-term Ambition XP to one player', () => {
+    // @ts-expect-error type
+    globalThis.document = {
+      getElementById: (id: string) => {
+        const element = {
+          value: '',
+        };
+        const shortTermID = 'short-term-reason';
+
+        if (id === shortTermID) {
+          element.value = 'Short-term Ambition';
+        }
+
+        if (id !== shortTermID) {
+          return null;
+        }
+
+        return element as HTMLInputElement;
+      },
+
+      querySelector: (selector: string) => {
+        const element = {
+          value: '',
+        };
+
+        if (selector === 'input[name="xp"]:checked') {
+          element.value = '50';
+        }
+
+        return element as HTMLInputElement;
+      },
+    };
+
+    // @ts-expect-error type
+    const token: Token = {
+      actor: {
+        details: {
+          experience: {
+            current: 100,
+            total: 200,
+          },
+        },
+        awardExp: (XP: number, reason: string) => {
+          expect(XP).toBe(50);
+          expect(reason).toBe('Short-term Ambition');
+        },
+      },
+    } as Token;
+    const selectedPlayers: Token[] = [token];
+
+    addXP(selectedPlayers);
+  });
+
+  it('add long-term Ambition XP to one player', () => {
+    // @ts-expect-error type
+    globalThis.document = {
+      getElementById: (id: string) => {
+        const element = {
+          value: '',
+        };
+        const longTermID = 'long-term-reason';
+
+        if (id === longTermID) {
+          element.value = 'Long-term Ambition';
+        }
+
+        if (id !== longTermID) {
+          return null;
+        }
+
+        return element as HTMLInputElement;
+      },
+
+      querySelector: (selector: string) => {
+        const element = {
+          value: '',
+        };
+
+        if (selector === 'input[name="xp"]:checked') {
+          element.value = '500';
+        }
+
+        return element as HTMLInputElement;
+      },
+    };
+
+    // @ts-expect-error type
+    const token: Token = {
+      actor: {
+        details: {
+          experience: {
+            current: 100,
+            total: 200,
+          },
+        },
+        awardExp: (XP: number, reason: string) => {
+          expect(XP).toBe(500);
+          expect(reason).toBe('Long-term Ambition');
+        },
+      },
+    } as Token;
+    const selectedPlayers: Token[] = [token];
+
+    addXP(selectedPlayers);
+  });
 });
--- src/add-xp.ts
+++ src/add-xp.ts
@@ -4,18 +4,49 @@ export const addXP = (selectedTokens: Token[]): void => {
     reason: game.i18n.localize('wfrp4e.add-xp.modal.value.defaults.reason'),
   };
 
-  const XPInput: HTMLInputElement = document.getElementById(
-    'xp',
-  ) as HTMLInputElement;
-  const XP = parseInt(XPInput?.value) || defaults.XP;
+  const getXPInput = (): HTMLInputElement | null => {
+    const ID = 'xp';
+    const selector = `input[name="${ID}"]:checked`;
 
-  const value = document.querySelector('input[name="xp"]:checked')?.value;
-  console.log({ value });
+    return (
+      (document.getElementById(ID) as HTMLInputElement) ||
+      (document.querySelector(selector) as HTMLInputElement)
+    );
+  };
 
-  const reasonInput: HTMLInputElement = document.getElementById(
-    'reason',
-  ) as HTMLInputElement;
-  const reason = reasonInput?.value || defaults.reason;
+  const getValue = (input: HTMLInputElement | null): number => {
+    const valueAsString = String(input?.value) || '';
+
+    return parseInt(valueAsString) || defaults.XP;
+  };
+
+  const XPInput: HTMLInputElement | null = getXPInput();
+  const XP: number = getValue(XPInput);
+
+  const getReasonInput = (XP: number | null): HTMLInputElement | null => {
+    const ID = mapXPToReasonID(XP);
+    const reasonInput: HTMLInputElement = document.getElementById(
+      ID,
+    ) as HTMLInputElement;
+
+    return reasonInput;
+  };
+
+  const mapXPToReasonID = (XP: number | null): string => {
+    // INFO: Prawdpodobnie mapowanie po XP === 50 nie jest dobry rowiazaniem.
+    // Jest to short term solution.
+    if (XP === 50) {
+      return 'short-term-reason';
+    }
+
+    if (XP === 500) {
+      return 'long-term-reason';
+    }
+
+    return 'reason';
+  };
+
+  const reason = getReasonInput(XP)?.value || defaults.reason;
 
   // TODO: Add token types
   selectedTokens.forEach((token) => {
