aa68f4af59f50bace961ffc511abe3f88a61c59d
th3mon <p.szelenberger@gmail.com>
2023-12-31 02:46:06 +0100

    Remove unneeded querySelector() mock

--- src/add-xp.test.ts
+++ src/add-xp.test.ts
@@ -32,18 +32,6 @@ describe('addXP', () => {
 
         return element as HTMLInputElement;
       },
-
-      querySelector: (selector: string) => {
-        const element = {
-          value: '',
-        };
-
-        if (selector === 'input[name="xp"]:checked') {
-          element.value = '100';
-        }
-
-        return element as HTMLInputElement;
-      },
     };
 
     // @ts-expect-error type
@@ -78,18 +66,6 @@ describe('addXP', () => {
           element.value = '';
         } else if (id === 'reason') {
           element.value = '';
-        }
-
-        return element as HTMLInputElement;
-      },
-
-      querySelector: (selector: string) => {
-        const element = {
-          value: '',
-        };
-
-        if (selector === 'input[name="xp"]:checked') {
-          element.value = '100';
         }
 
         return element as HTMLInputElement;
